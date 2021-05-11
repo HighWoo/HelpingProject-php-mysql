06faeb141fc04f43e4a18b7f80e2efb7c37a7040
manapi97 <rkdwodms86@naver.com>
2021-05-11 17:31:47 +0900

    5/11 재은이

---
+++ Vmat.php
@@ -0,0 +1,27 @@
+<?php
+	$id = $_POST['id'];
+	$time = $_POST['time'];
+	$work = $_POST['work'];
+	$location = $_POST['location'];
+  if($id==null||$time==null||$work==null||$location==null){
+	print "<script language=javascript> alert('빈칸이있습니다'); location.replace('User.php'); </script>";
+}
+
+	$connect = mysqli_connect('localhost',  'root', '123456');
+	mysqli_select_db($connect, 'umat');
+	
+    /*  --------    PHP와 MySQL 한글 깨짐 방지  --------   */
+
+	mysqli_query($connect, "set session character_set_connection=utf8;");		 
+	mysqli_query($connect, "set session character_set_results=utf8;");		 
+	mysqli_query($connect, "set session character_set_client=utf8;");		
+
+	$sql="insert into umat (SeekerID ,SeekerTime ,VolunteerWork ,ServiceLocation) ";
+	$sql.= "values('$id', '$time','$work','$location')";
+
+	mysqli_query($connect, $sql) ;        //sql 질의 수행.
+	mysqli_close($connect);                 //db 연결 종료
+	
+print "<script language=javascript> alert('게시판 등록이 완료되었습니다.'); location.replace('alogmain.html'); </script>";
+
+?>
