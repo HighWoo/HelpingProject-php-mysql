d79aeafc904e96f13cc0aa24a1dc83bd00e574a7
manapi97 <rkdwodms86@naver.com>
2021-06-05 19:50:41 +0900

    Update volcontents.sql

--- database/volcontents.sql
+++ database/volcontents.sql
@@ -5,6 +5,7 @@ volclass varchar(50) not null,
 volcontent varchar(50) not null,
 voltime int(11) not null,
 volnum int(11) not null,
+id varchar(20) not null,
 primary key(volnum)
 )
 default charset=utf8;
