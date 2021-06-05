fe78944fbb7a02be00785ea1cd51e7cd78e1202c
manapi97 <rkdwodms86@naver.com>
2021-06-05 20:05:44 +0900

    Update vollookup.php

--- vollookup.php
+++ vollookup.php
@@ -7,8 +7,7 @@
 	include "connect_db.php";
 	$sql = "select * from volcontents";  
 	$result = mysqli_query ($connect, $sql);          
-	$count = mysqli_num_fields($result);
-	mysqli_close($connect);   
+	$count = mysqli_num_fields($result);   
 
 ?>
 
@@ -129,19 +128,11 @@
                 $currentPage = $_GET["currentPage"];
             }
  
-            //mysqli_connect()함수로 커넥션 객체 생성
-            $conn = mysqli_connect("localhost", "root");
-            //커넥션 객체 생성 확인
-            if($conn) {
-                echo "연결 성공<br>";
-            } else {
-                die("연결 실패 : " .mysqli_error());
-            }
-            mysqli_select_db($conn,'Login');
+            
             //페이징 작업을 위한 테이블 내 전체 행 갯수 조회 쿼리
             $sqlCount = "SELECT count(*) FROM volcontents";
 			
-            $resultCount = mysqli_query($conn,$sqlCount);
+            $resultCount = mysqli_query($connect,$sqlCount);
 		
             if($rowCount = mysqli_fetch_array($resultCount)){
                 $totalRowNum = $rowCount["count(*)"];   //php는 지역 변수를 밖에서 사용 가능.
@@ -150,7 +141,7 @@
             if($resultCount) {
                 echo "행 갯수 조회 성공 : ". $totalRowNum."<br>";
             } else {
-                echo "결과 없음: ".mysqli_error($conn);
+                echo "결과 없음: ".mysqli_error($connect);
             }
                         
             $rowPerPage = 5;   //페이지당 보여줄 게시물 행의 수
@@ -158,12 +149,12 @@
             //board 테이블을 조회해서 board_no, board_title, board_user, board_date 필드 값을 내림차순으로 정렬하여 모두 가져 오는 쿼리
             //입력된 begin값과 rowPerPage 값에 따라 가져오는 행의 시작과 갯수가 달라지는 쿼리
             $sql = "SELECT * FROM volcontents order by volnum desc limit ".$begin.",".$rowPerPage."";
-            $result = mysqli_query($conn,$sql);
+            $result = mysqli_query($connect,$sql);
             //쿼리 조회 결과가 있는지 확인
             if($result) {
                 echo "조회 성공";
             } else {
-                echo "결과 없음: ".mysqli_error($conn);
+                echo "결과 없음: ".mysqli_error($connect);
             }
         ?>
         <table class="table table-bordered">
@@ -229,7 +220,7 @@
                 //다음 버튼이 클릭될때 GET방식으로 currentPage변수 값에 1을 더한 값이 넘어가도록 함
                 echo "<a class='btn btn-primary' href='/board_list.php?currentPage=".($currentPage+1)."'>다음</a>";
             }
-            mysqli_close($conn);
+            mysqli_close($connect);
         ?>
         &nbsp;&nbsp;
         <a class="btn btn-primary" href="/board_add_form.php">글 쓰기</a>
