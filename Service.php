32e7bcb2c7626af00e30b6ff0ab322627ad3346f
manapi97 <rkdwodms86@naver.com>
2021-06-05 19:12:54 +0900

    재은

--- Service.php
+++ Service.php
@@ -7,6 +7,33 @@
  <link rel="stylesheet" href="files/main.css">
   <link rel="icon" type="image/png" href="files/smalllogo.png">
  </head>
+ <style>
+        table{
+                border-top: 1px solid #444444;
+                border-collapse: collapse;
+        }
+        tr{
+                border-bottom: 1px solid #444444;
+                padding: 10px;
+        }
+        td{
+                border-bottom: 1px solid #efefef;
+                padding: 10px;
+        }
+        table .even{
+                background: #efefef;
+        }
+        .text{
+                text-align:center;
+                padding-top:20px;
+                color:#000000
+        }
+        .text:hover{
+                text-decoration: underline;
+        }
+        a:link {color : #57A0EE; text-decoration:none;}
+        a:hover { text-decoration : underline;}
+</style>
 <body>
 <div id="home" class="big-bg">
  <header class="page-header wrapper">
@@ -22,6 +49,51 @@
    </nav>
    </header>
   <H1> 고객센터 페이지 </H1>
+  <?php
+  include ("connect_db.php");
+  $query ="select * from question order by number desc";
+  $result = $connect->query($query);
+  $total = mysqli_num_rows($result);
+  ?>
+  <h2 align=center>문의 게시판</h2>
+  <h3 align=center>이곳은 자원봉사자나 사용자의 궁금한 사항을 문의를 하는 곳 입니다.</h3>
+        <table align = center>
+        <thead align = "center">
+        <tr>
+        <td width = "50" align="center">번호</td>
+        <td width = "500" align = "center">제목</td>
+        <td width = "100" align = "center">작성자</td>
+        <td width = "200" align = "center">날짜</td>
+        </tr>
+        </thead>
+        
+        <tbody>
+        <?php
+                while($rows = mysqli_fetch_assoc($result)){ 
+                        if($total%2==0){
+        ?>                      <tr class = "even">
+                        <?php   }
+                        else{
+        ?>                      <tr>
+                        <?php } ?>
+                <td width = "50" align = "center"><?php echo $total?></td>
+                <td width = "500" align = "center">
+                <a href = "question_view.php?number=<?php echo $rows['number']?>">
+                <?php echo $rows['title']?></td>
+                  <td width = "100" align = "center"><?php echo $rows['id']?></td>
+                <td width = "200" align = "center"><?php echo $rows['date']?></td>
+                </tr>
+        <?php
+                $total--;
+                }
+        ?>
+        </tbody>
+        </table>
+ 
+        <div class = text>
+        <font style="cursor: hand"onClick="location.href='./question_write.php'">글쓰기</font>
+        </div>
+
 
 	</div>
 
