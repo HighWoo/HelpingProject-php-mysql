e8b42ec72bab3e86f239993f51acc03d51205f0b
manapi97 <rkdwodms86@naver.com>
2021-06-07 12:49:25 +0900

    뷰

---
+++ question_view.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html>
+<head>
+ <meta charset="UTF-8">
+ <title>Helping</title>
+ <meta name="description" content="ㅁㅁㅁ">
+ <link rel="stylesheet" href="files/main.css">
+  <link rel="icon" type="image/png" href="files/smalllogo.png">
+ </head>
+ 
+<style>
+        table.table2{
+                border-collapse: separate;
+                border-spacing: 1px;
+                text-align: left;
+                line-height: 1.5;
+                border-top: 1px solid #ccc;
+                margin : 20px 10px;
+        }
+        table.table2 tr {
+                width: 50px;
+                padding: 10px;
+                font-weight: bold;
+                vertical-align: top;
+                border-bottom: 1px solid #ccc;
+        }
+        table.table2 td {
+                 width: 100px;
+                 padding: 10px;
+                 vertical-align: top;
+                 border-bottom: 1px solid #ccc;
+                 
+                
+        }
+ 
+</style>
+
+ 
+ <body>
+<div id="home" class="big-bg">
+ <header class="page-header wrapper">
+  <h1><a href="blogmain.html"><img class="logo" src="files/mainlogo.svg" alt="사이트 홈"></a></h1>
+  <nav>
+   <ul class="main-nav">
+   <li><a href="Guide.html">소개</a></li>
+   <li><a href="Login.php">구인구직</a></li>
+   <li><a href="Vinfor.php">봉사현황</a></li>
+   <li><a href="Service.php">고객센터</a></li>
+   <li><a href="informod.php">내정보</a></li>
+   <li><a href="blogmain.html" onclick="alert('로그아웃 되었습니다');">로그아웃</a></li>
+   </ul>
+   </nav>
+   </header>
+   
+   <?php
+   include "connect_db.php";
+   
+    $number = $_GET['number'];
+    session_start();
+    $query = "select title, content, id, date from question where number =$number";
+    $result = $connect->query($query);
+    $rows = mysqli_fetch_assoc($result);
+        ?>
+ 
+        <table  style="padding-top:50px" align = center width=700 border=0 cellpadding=2 >
+                <tr>
+                <td height=10 align= center bgcolor=#ccc><font color=black>제목 : <?php echo $rows['title']?></font></td>
+                </tr>
+                <tr>
+                <td bgcolor=white>
+                <table class = "table2">
+                        <tr>
+                        <td>내용</td>
+                        <td><?php echo $rows['content']?></td>
+                        </tr>
+                        </table>
+ 
+                        <center>
+                        <div class="view_btn">
+                          <button onclick="location.href='./question_reply.php?number=<?=$number?>&id=<?=$_SESSION['id']?>'">답글쓰기</button>
+                          <button onclick="location.href='./Service.php'">목록으로</button>
+                          <button onclick="location.href='./question_modify.php?number=<?=$number?>&id=<?=$_SESSION['id']?>'">수정</button>
+ 
+                          <button onclick="location.href='./question_delete.php?number=<?=$number?>&id=<?=$_SESSION['id']?>'">삭제</button>
+                        </div>
+                        </center>
+                </td>
+                </tr>
+        </table>
+ 
+ 
+        
+        
+   
+   
+  </body>
+  </html>
