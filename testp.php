12f8d27ebd16f58897613e1e8a9b18f862c95e04
manapi97 <rkdwodms86@naver.com>
2021-06-12 16:57:34 +0900

    t

--- testp.php
+++ testp.php
@@ -1,6 +1,5 @@
 <?php
 
-session_start(); 
 include ("connect_db.php"); 
 
 ?>
@@ -24,21 +23,7 @@ include ("connect_db.php");
 <body>
 <form name=appvol method=post action="app_db.php">
 <div id="home" class="big-bg">
- <header class="page-header wrapper">
-  <h1><a href="alogmain.php"><br><img class="logo" src="files/mainlogo.svg" alt="사이트 홈"></a></h1>
-  <nav>
-   <ul class="main-nav">
-
-    <li><a href="aGuide.html">소개  </a></li>
-   <li><a href="notice.php">구인구직</a></li>
-   <li><a href="Vinfor.php">봉사현황</a></li>
-   <li><a href="Service.html">고객센터</a></li>
-    <li><a href="informod.php">내정보</a></li>
-  <li><a href="blogmain.html" onclick="alert('로그아웃 되었습니다');">로그아웃</a></li>
-
-   </ul>
-   </nav>
-   </header>
+ <?php include "header.php" ?>
  <div id="board_area"> 
   <center><br><br><br><h1><?php echo $_SESSION['id']; ?> 님이 지원하신 봉사활동</h1>
   <h4>원하는 봉사활동의 제목을 클릭하여 상세정보를 확인하세요</h4></center>
