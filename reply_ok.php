47ee81a256ba4196c3300f6ed2e730efa3f3414a
manapi97 <rkdwodms86@naver.com>
2021-06-12 22:08:59 +0900

    Update reply_ok.php

--- reply_ok.php
+++ reply_ok.php
@@ -17,14 +17,10 @@ $content=$_POST['content'];
 date_default_timezone_set('Asia/Seoul');
 $date = date('Y-m-d H:i:s');
 $question_user=$_POST['question_user'];
-
 if($content==null){
 	print "<script language=javascript> alert('빈칸이있습니다'); location.replace('question_view.php?number=$board_number'); </script>";
 }
-else if($id!='super'||$id!=$question_user){
-  	print "<script language=javascript> alert('관리자와 질문유저만 댓글을 남길 수 있습니다.'); location.replace('question_view.php?number=$board_number'); </script>";
-  }
-  else {
+  else if ($id==$question_user||$id=='super') {
     
     
 $query = "insert into comment (number,board_number, id, content, date) values(null,'$board_number', '$id', '$content','$date')";
@@ -39,6 +35,10 @@ $result = $connect->query($query);
                 mysqli_close($connect);
                 
       }
+      else {
+  	print "<script language=javascript> alert('관리자와 질문유저만 댓글을 남길 수 있습니다.'); location.replace('question_view.php?number=$board_number'); </script>";
+  }
+      
 ?>
 
   </body>
