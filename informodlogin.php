de0872c0b0933e7cc5d7ebc0a6d44426995b6747
manapi97 <rkdwodms86@naver.com>
2021-06-12 16:34:15 +0900

    Update informodlogin.php

--- informodlogin.php
+++ informodlogin.php
@@ -19,28 +19,10 @@
  <center>
 <body>
 <div id="home" class="big-bg">
- <header class="page-header wrapper">
-  <h1><a href="blogmain.html"><br><img class="logo" src="files/mainlogo.svg" alt="사이트 홈"></a></h1>
-  <nav>
-   <ul class="main-nav">
-    <li><a href="aGuide.html">소개</a></li>
-   <li><a href="notice.php">구인구직</a></li>
-   <li><a href="Vinfor.php">봉사현황</a></li>
-   <li><a href="Service.php">고객센터</a></li>
-    <li><a href="informod.php">내정보</a></li>
-  <li><a href="blogmain.html" onclick="alert('로그아웃 되었습니다');">로그아웃</a></li>
-
-   </ul>
-   </nav>
-   </header>
+ <?php include "header.php" ?>
+ 
 <h1 >[내정보]</h1>
 <HR size=4>
-<?php
-
-session_start();
-
-
-?>
 <UL type="disc">
 </UL>
 <strong> 비밀번호를 입력해 본인인증 을해주세요.</strong> 
