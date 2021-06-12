ef838d49ede6882db288c5e67cd0e2a05217c663
manapi97 <rkdwodms86@naver.com>
2021-06-12 16:37:04 +0900

    Update Login.php

--- Login.php
+++ Login.php
@@ -10,29 +10,11 @@
  </head>
 <body>
 <div id="home" class="big-bg">
- <header class="page-header wrapper">
-  <h1><a href="blogmain.html"><br><img class="logo" src="files/mainlogo.svg" alt="사이트 홈"></a></h1>
-  <nav>
-   <ul class="main-nav">
-   <li><a href="Guide.html">소개</a></li>
-   <li><a href="login.php">구인구직</a></li>
-   <li><a href="Vinfor.php">봉사현황</a></li>
-   <li><a href="Service.html">고객센터</a></li>
-   <li><a href="login.php">로그인</a></li>
-
-   </ul>
-   </nav>
-   </header>
+ <?php include "header.php" ?>
    
    <div class="home-content wrapper">
    
 	
-<?php
-
-session_start();
-
-
-?>
 <form name="login_form" action="logcheck.php" method="post"><br><br><br>
  <INPUT type="text" name="userid" placeholder="아이디" style="WIDTH: 170pt; HEIGHT: 20pt;  background:#9ba5a0; font-size:1.2em;"><BR><BR>
  <INPUT type="password" name="userpw" placeholder="비밀번호" style="WIDTH: 170pt; HEIGHT: 20pt;  background:#9ba5a0; font-size:1.2em;"><BR><BR><BR>
