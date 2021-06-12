0ec878b6f0381dc869a0938110eb6848a29de997
manapi97 <rkdwodms86@naver.com>
2021-06-12 16:37:10 +0900

    Update matchingend_user.php

--- matchingend_user.php
+++ matchingend_user.php
@@ -1,6 +1,5 @@
 <?php
 
-session_start(); 
 include ("connect_db.php"); 
 
 ?>
@@ -24,21 +23,8 @@ include ("connect_db.php");
 <body>
 <form name=appvol method=post action="app_db.php">
 <div id="home" class="big-bg">
- <header class="page-header wrapper">
-  <h1><a href="alogmain.php"><br><img class="logo" src="files/mainlogo.svg" alt="사이트 홈"></a></h1>
-  <nav>
-   <ul class="main-nav">
-
-    <li><a href="aGuide.html">소개  </a></li>
-   <li><a href="notice.php">구인구직</a></li>
-   <li><a href="Vinfor.php">봉사현황</a></li>
-   <li><a href="Service.html">고객센터</a></li>
-    <li><a href="informod.php">내정보</a></li>
-  <li><a href="blogmain.html" onclick="alert('로그아웃 되었습니다');">로그아웃</a></li>
-
-   </ul>
-   </nav>
-   </header>
+ <?php include "header.php" ?>
+ 
  <div id="board_area"> 
    <?php
 		  $nums=$_GET['volnum'];
